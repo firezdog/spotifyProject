fe530550d9930d8c65a0473ffe8db5be5ea8ca26
coleanderson98604 <shuffle2dabeat@gmail.com>
2018-03-27 15:57:51 -0500

    Added cool logo

--- apps/spotify_app/static/spotifyJS/index.js
+++ apps/spotify_app/static/spotifyJS/index.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
                     console.log(imageRes)
                     let htmlString = ""
                     htmlString+="<div class='column'>"
-                    htmlString+=`<div class='ui center aligned segment'><img src=${imageRes.images[0].url}></div>`
+                    htmlString+=`<div class='ui center aligned segment'><a href="/spotify/${item[i].id}"><img src=${imageRes.images[0].url}></a></div>`
                     htmlString+="<div class='item'>Name: " + item[i]['name'] + '</div>'
                     htmlString+="<div class='item'>Album: " + item[i]['albumName'] + '</div>'
-                    htmlString+="<div>Artist: " + item[i]['artistName'] + ' </div>'
+                    htmlString+="<div class='item'>Artist: " + item[i]['artistName'] + ' </div>'
                     htmlString+="</div>"    
                     console.log(`#genre${index}`)
                     $(`#genre${index}`).append(htmlString);
