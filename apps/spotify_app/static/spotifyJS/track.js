e62c697e068c25b337a52c74eb99f071731a432d
coleanderson98604 <shuffle2dabeat@gmail.com>
2018-03-30 10:23:51 -0500

    Added liked stuff

--- apps/spotify_app/static/spotifyJS/track.js
+++ apps/spotify_app/static/spotifyJS/track.js
@@ -43,12 +43,17 @@ $(document).ready(function() {
     $.get(`http://api.napster.com/v2.2/tracks/${track_id}?apikey=${key}`, function(track) {
         $.get(`https://api.napster.com/v2.2/albums/${track.tracks[0].albumId}/images?apikey=${key}`, function(album) {
             $("#album-picture").attr("src",album.images[2].url);
+            $("#artist-name").text(track.tracks[0].artistName)
+            $("#track-title").text(track.tracks[0].name)
+            $("#album-title").text(track.tracks[0].albumName)
+            console.log(track.tracks[0].previewURL)
+            $.get(`/spotify/tracks/tracks/${track_id}/likebutton`).done(function(likebutton){
+                let htmlString = ""
+                htmlString+= likebutton
+                $('#likebutton').append(htmlString)
+            })
         });
         
-        $("#artist-name").text(track.tracks[0].artistName)
-        $("#track-title").text(track.tracks[0].name)
-        $("#album-title").text(track.tracks[0].albumName)
-        console.log(track.tracks[0].previewURL)
         $("#audio").html(`<source src='${track.tracks[0].previewURL}' type='audio/mp3'> Your browser does not support the audio element.'`)
         $.get(`http://api.napster.com/v2.2/artists/${track.tracks[0].artistId}/similar?apikey=${key}` , function(similar){
             for(let i=0; i < 25; i++){
